fc4a125403faf90b53311421cb65c58159e5917d
Javier Santiago Salazar <100961042+JavierSantiagoSalazar@users.noreply.github.com>
2023-04-29 15:19:58 -0500

    feature/main_activity (#1)
    * Initial main view creation
    
    * Finish MainView and begin the HomeView

--- app/src/main/java/com/javierestudio/instaflixapp/MainActivity.kt
+++ app/src/main/java/com/javierestudio/instaflixapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.javierestudio.instaflixapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.javierestudio.instaflixapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.toolbar.setOnMenuItemClickListener {menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_settings -> {
+                    println("Incoming action")
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
