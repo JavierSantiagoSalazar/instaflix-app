e35ae686981a6c2e8209e98efb7d28a35e316e28
Javier Santiago Salazar <javiersantiago45011@gmail.com>
2023-04-30 23:38:46 -0500

    Navigation With BottomNavView and Ui Fixes

--- app/src/main/java/com/javierestudio/instaflixapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/javierestudio/instaflixapp/ui/main/MainActivity.kt
@@ -2,6 +2,11 @@ package com.javierestudio.instaflixapp.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.javierestudio.instaflixapp.R
 import com.javierestudio.instaflixapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,8 +21,15 @@ class MainActivity : AppCompatActivity() {
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        mBinding.toolbar.setOnMenuItemClickListener {menuItem ->
+        val bottomNav = mBinding.bottomNavigationView
+
+        NavigationUI.setupWithNavController(bottomNav, navController)
+
+        mBinding.toolbar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.action_settings -> {
                     println("Incoming action")
